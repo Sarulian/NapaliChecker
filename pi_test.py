67cf801682108d06704b9380f8b4f964347175bf
jecd <jecrivellidecker@ucdavis.edu>
2020-07-03 21:40:40 -0700

    trying firefox

--- pi_test.py
+++ pi_test.py
@@ -4,16 +4,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
+from pyvirtualdisplay import Display
 import time
 
 
 if __name__ == '__main__':
-	driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')  # Optional argument, if not specified will search path.
-	driver.get('http://www.google.com/xhtml');
-	time.sleep(5) # Let the user actually see something!
-	search_box = driver.find_element_by_name('q')
-	search_box.send_keys('ChromeDriver')
-	search_box.submit()
-	time.sleep(5) # Let the user actually see something!
-	driver.quit()
+	driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
+	driver.get('https://www.google.com')
+	print(driver.page_source)
+	driver.close()
+	display.stop()
+	# driver = webdriver.Chrome('/usr/lib/chromium-browser/chromedriver')  # Optional argument, if not specified will search path.
+	# driver.get('http://www.google.com/xhtml');
+	# time.sleep(5) # Let the user actually see something!
+	# search_box = driver.find_element_by_name('q')
+	# search_box.send_keys('ChromeDriver')
+	# search_box.submit()
+	# time.sleep(5) # Let the user actually see something!
+	# driver.quit()
 
