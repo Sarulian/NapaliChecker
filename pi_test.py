2a04c8d37cfca0d3562d667a844caed49ae459b1
jecd <jecrivellidecker@ucdavis.edu>
2020-07-08 11:38:26 -0700

    trying test with ted changes

--- pi_test.py
+++ pi_test.py
@@ -2,13 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from pyvirtualdisplay import Display
 import time
 
 
 if __name__ == '__main__':
+
+	opts = Options()
+	opts.headless = True
 	driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
 	driver.get('https://www.google.com')
 	print(driver.page_source)
